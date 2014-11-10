fd9d11e6c015b96eeeeb4a27652d92eb5b000330
Gabriel Such <gsuch@salesforce.com>
2014-11-10 14:02:21 -0800

    more some exercises: abs, divides? and fizzbuzz

--- src/training_day.clj
+++ src/training_day.clj
@@ -15,3 +15,16 @@
 
 (defn teen? [age]
   (and (> age 12) (< age 20)))
+
+(defn abs [x]
+  (if (< x 0) (* x -1) x))
+
+(defn divides? [divisor n]
+  (= (mod n divisor) 0))
+
+(defn fizzbuzz [n]
+  (cond
+   (= (mod n 15) 0) "gotcha!"
+   (= (mod n 5) 0) "buzz"
+   (= (mod n 3) 0) "fizz"
+   :else ""))
--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -16,3 +16,19 @@
   (teen? 19) => true
   (teen? 20) => false
   (teen? 27) => false)
+
+(facts "abs"
+  (abs -2) => 2
+  (abs 42) => 42)
+
+(facts "divides?"
+  (divides? 2 4) => true
+  (divides? 4 2) => false
+  (divides? 5 10) => true
+  (divides? 2 5) => false)
+
+(facts "fizzbuzz"
+  (fizzbuzz 2)  => ""
+  (fizzbuzz 45) => "gotcha!"
+  (fizzbuzz 48) => "fizz"
+  (fizzbuzz 70) => "buzz")
